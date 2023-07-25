d5f7b4033a0f10108d15b65fd31c8b4085cc711f
Luciano Andrade <luciano.andrade@compasso.com.br>
2023-07-25 11:39:44 -0300

    fim do segundo dia de curso

--- src/components/BoxMain.vue
+++ src/components/BoxMain.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="box has-text-weight-bold">
+  <div class="box has-text-weight-bold" :style="estilos">
     <slot></slot>
   </div>
 </template>
@@ -7,12 +7,13 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 export default defineComponent({
-  name: 'BoxMain'
+  name: 'BoxMain',
+  data () {
+    return {
+      estilos: {
+        backgroundColor: '#FAF0CA'
+      }
+    }
+  }
 })
 </script>
-
-<style scoped>
-.box {
-  background: #FAF0CA;
-}
-</style>
